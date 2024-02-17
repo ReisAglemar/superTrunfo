eab8bffe75bae1635c89e75d169355eb7e7d672a
Reis <aglemar.reis@gmail.com>
2024-02-17 16:03:02 -0300

    created class to play

---
+++ supertrunfo/src/main/java/entities/Toplay.java
@@ -0,0 +1,37 @@
+package entities;
+
+/**
+ *
+ * @author reis
+ */
+public class Toplay implements ActionsInterface{
+
+    @Override
+    public void chooseAttributeCard(DeskCards cards) {
+    }
+
+    @Override
+    public void earnCard(DeskCards cards) {
+    }
+
+    @Override
+    public void loseCard(DeskCards cards) {
+    }
+
+    @Override
+    public void celebrateVictory() {
+    }
+
+    @Override
+    public void regretDefeat() {
+    }
+
+    @Override
+    public void howManyCards() {
+    }
+
+    @Override
+    public void defineWinner() {
+    }
+    
+}
