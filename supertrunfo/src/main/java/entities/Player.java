12204e5a2eadd4af2fe96cd03c9b15dea6571c94
Reis <aglemar.reis@gmail.com>
2024-02-17 16:02:06 -0300

    definined class Player

--- supertrunfo/src/main/java/entities/Player.java
+++ supertrunfo/src/main/java/entities/Player.java
@@ -4,6 +4,61 @@ package entities;
  *
  * @author reis
  */
-public class Player {
+public class Player implements ActionsInterface{
+    
+    private String nane;
+    int id;
+
+    public Player(String nane, int id) {
+        this.nane = nane;
+        this.id = id;
+    }
+
+    public String getNane() {
+        return nane;
+    }
+
+    public void setNane(String nane) {
+        this.nane = nane;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void chooseAttributeCard(DeskCards cards) {
+    }
+
+    @Override
+    public void earnCard(DeskCards cards) {
+    }
+
+    @Override
+    public void loseCard(DeskCards cards) {
+    }
+
+    @Override
+    public void celebrateVictory() {
+    }
+
+    @Override
+    public void regretDefeat() {
+    }
+
+    @Override
+    public void howManyCards() {
+    }
+
+    @Override
+    public void defineWinner() {
+    }
+    
+    
+    
     
 }
