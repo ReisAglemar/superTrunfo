2c5cc03020ffc17d96e0aa1ea3d8f09dbb186331
Reis <aglemar.reis@gmail.com>
2024-02-17 23:17:25 -0300

    change and add methods

--- supertrunfo/src/main/java/entities/ActionsInterface.java
+++ supertrunfo/src/main/java/entities/ActionsInterface.java
@@ -9,6 +9,7 @@ public interface ActionsInterface {
     public abstract void chooseAttributeCard(DeskCards cards);
     public abstract void earnCard(DeskCards cards);
     public abstract void loseCard(DeskCards cards);
+    public DeskCards[] shuffle(DeskCards[] cards);
     public abstract void celebrateVictory();
     public abstract void regretDefeat();
     public abstract void howManyCards();
